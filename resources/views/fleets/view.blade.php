f90aac091ab0e5b7b59c24e0463c24d9621aa0aa
fordav3 <fordav3@gmail.com>
2019-07-01 17:44:16 -0500

    Update view.blade.php

--- resources/views/fleets/view.blade.php
+++ resources/views/fleets/view.blade.php
@@ -39,6 +39,9 @@
     <div class="box box-primary box-solid">
         <div class="box-header">
             <h3>Participants</h3>
+            <button type="button" class="btn btn-xs btn-box-tool" id="addLoot" data-toggle="modal" data-target="#addLootModal" data-placement="top" title="Add a new fitting">
+                <span class="fa fa-plus-square"></span>
+            </button>
         </div>
         <div class="box-body">
             <table class="table">
